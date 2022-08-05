59b15a64edc8dad7439d552a85d8a66d56c0a813
dan-studio <danstudiocode@gmail.com>
2022-08-05 19:28:26 +0900

    day1

--- 4.sum-between-two-integers.js
+++ 4.sum-between-two-integers.js
@@ -1,18 +1,9 @@
 function solution(a, b) {
   var answer = 0;
-  var sub = Math.abs(a - b) - 1
-  if (a < b) {
-    for (i = 0; i < sub; i++) {
-      return a + 1
-    }
-    return a + b
-  } else if (a == b) {
-    return a
-  } else {
-    for (i = 0; i < sub; i++) {
-      return b + (b + 1) + a
-    }
+  var max = Math.max(a,b)
+  var min = Math.min(a,b)
+  for(let i=min; i<=max;i++){
+      answer +=i
   }
-  // return answer
-  console.log(answer)
+  return answer
 }
